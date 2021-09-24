24f91d74de7fdabc2509dd7dde5a3274093580aa
Ajay <ajaysudani07@gmail.com>
2021-09-24 23:35:46 +0530

    Updated gatsby-node.js

--- plugins/gatsby-source-users/gatsby-node.js
+++ plugins/gatsby-source-users/gatsby-node.js
@@ -1,11 +1,10 @@
 const { request, gql } = require("graphql-request");
 
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest },
+  { actions, createContentDigest },
   { userName }
 ) => {
   const API_ENDPOINT = "https://api.hashnode.com/";
-  const { createNode } = actions;
 
   // pass the variable into the query
   const query = gql`
@@ -30,21 +29,18 @@ exports.sourceNodes = async (
       publication: { posts },
     },
   } = response;
+
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
-    const node = {
-      id: createNodeId(post._id),
-      parent: null,
-      children: [],
+    actions.createNode({
+      id: post._id,
       internal: {
         type: `HashnodePost`,
-        mediaType: `application/json`, // https://www.gatsbyjs.com/docs/reference/graphql-data-layer/node-interface/#mediatype
         content: JSON.stringify(post), // https://www.gatsbyjs.com/docs/reference/graphql-data-layer/node-interface/#content
         contentDigest: createContentDigest(post),
       },
       ...post,
-    };
-    createNode(node);
+    });
   }
   return;
 };
